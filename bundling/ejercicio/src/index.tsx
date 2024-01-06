db7ff1aa1faba21ad8d87b2d0d02ccd3f310ce93
Iván Ruiz <ivr.644@gmail.com>
2024-01-06 15:26:16 +0100

    Added bundling/ejercicio

---
+++ bundling/ejercicio/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import logo from "./assets/logo.png";
+import "./styles.scss";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>Hello World 🌍</h1>
+    <img src={logo} style={{ width: 300 }} />
+  </div>
+);
